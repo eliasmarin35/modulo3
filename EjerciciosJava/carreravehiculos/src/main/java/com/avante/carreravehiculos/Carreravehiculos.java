f4aa633dbc89b78a66ed4e55411bfd9d04ac8d7d
Juan Manuel <jmpinero@avante.es>
2025-10-06 13:54:56 +0200

    Carrera Vehiculos

---
+++ EjerciciosJava/carreravehiculos/src/main/java/com/avante/carreravehiculos/Carreravehiculos.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.avante.carreravehiculos;
+
+/**
+ *
+ * @author jprof
+ */
+public class Carreravehiculos {
+
+    /*
+        Se trata de una carrera en la que los participantes pueden elegir el
+            vehiculo que van a usar (cohe, moto o bicicleta).
+        
+        La carrera acaba cuando un participante llega a la meta.
+    
+        La distancia a la meta se establece al crear la Carrera.
+    
+        Cada tipo de vehiculo tiene una velocidad maxima y minima, cuando un
+            jugardor elige un vehiculo, se le asigna una velocidad aleatoria
+            entre el minimo y el maximo.
+    
+        La velocidad se mide en metros por iteracion.
+    
+        La distancia se mide en metros.
+    
+        En cada iteración debe aparecer la posicion de cada jugador con un mensaje
+            personalizado dependiendo del vehiculo que tenga.
+    
+        Al finalizar se muestra que jugador ha ganado para cada una de las categorias
+            (coche, moto o bicicleta).
+    
+    */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
