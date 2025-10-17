aa58df693d67679dadd29876b74aa09e9608cf3f
Juan Manuel <jmpinero@avante.es>
2025-10-17 10:01:48 +0200

    Prueba Conexion: Configuración en las propiedades

--- TeoriaJava/jdbc/PruebaConexionBD/src/main/java/com/avante/pruebaconexionbd/DriverDB.java
+++ TeoriaJava/jdbc/PruebaConexionBD/src/main/java/com/avante/pruebaconexionbd/DriverDB.java
@@ -4,29 +4,56 @@
  */
 package com.avante.pruebaconexionbd;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  *
  * @author jprof
  */
 public class DriverDB {
-    private final String protocol = "jdbc:postgresql";
-    private final String host = "localhost";
-    private final String database = "app_pruebaconjdbc";
-    private final String username = "app_pruebaconjdbc";
-    private final String password = "12345";
-    
-    private final int port = 5432;
-    
+    private final String protocol;
+    private final String host;
+    private final String database;
+    private final String username;
+    private final String password;
+    private final String port;
     private final String url;
+    
+    private static final String CONFIG="./config.properties"; 
     
     private static DriverDB instance = null;
     
     private DriverDB () {
-        this.url = String.format("%s://%s:%d/%s",protocol,host,port,database);
+        // Objeto properties
+        Properties prop = new Properties();
+        
+        // Leer el fichero de propiedades
+        try (
+            // Accedemos al archivo de propiedades
+            InputStream in = getClass().getClassLoader().getResourceAsStream(DriverDB.CONFIG);
+        ) {
+            prop.load(in);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(
+                "No se puede leer la configuración.\n" + e.getMessage());
+        }
+        
+        // Inicializar las variables con las propiedades
+        this.protocol = prop.getProperty("db.protocol");
+        this.host = prop.getProperty("db.host");
+        this.port = prop.getProperty("db.port");
+        this.database = prop.getProperty("db.database");
+        this.username = prop.getProperty("db.username");
+        this.password = prop.getProperty("db.password");
+        
+        // Se construye la URL
+        this.url = String.format("%s://%s:%s/%s",protocol,host,port,database);
     }
     
     /**
